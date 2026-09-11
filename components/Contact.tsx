e60cc571256fd5bc87a675d9968d446d96905bca
tranquangtra <tranquangtra1705@gmail.com>
2026-09-11 22:10:26 +0700

    Update contact information

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -41,7 +41,7 @@ return ( <section id="contact" className="relative py-24 md:py-32">
           href="mailto:tqtra@hcmus.edu.vn"
           className="text-zinc-900 hover:text-zinc-600 transition"
         >
-          tranquangtra1705@gmail.com
+          tqtra@hcmus.edu.vn
         </a>
       </div>
 
